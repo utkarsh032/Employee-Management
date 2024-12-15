8ce4aa88235ecc17153c99bb85646c2182c1b244
utkarsh032 <utkarshraj525@gmail.com>
2024-12-15 20:18:43 +0530

    Image Detact update

--- src/components/TableView.jsx
+++ src/components/TableView.jsx
@@ -7,14 +7,14 @@ export const TableView = ({ data, theme }) => (
           <th className="py-2 px-4">Name</th>
           <th className="py-2 px-4">Email</th>
           <th className="py-2 px-4">Department</th>
-          <th className="py-2 px-4">DOJ</th>
+          <th className="py-2 px-4">DOB</th>
           <th className="py-2 px-4">Salary</th>
         </tr>
       </thead>
       <tbody>
         {data.map(([id, ele]) => (
           <tr key={id} className="text-center border-b hover:bg-gray-700">
-            <td><img src={ele.imageUrl || "https://via.placeholder.com/150"} alt={ele.name} className="w-12 h-12 rounded-full" /></td>
+            <td><img src={ele.imageUrl || "https://img.lovepik.com/free-png/20220125/lovepik-artificial-intelligence-png-image_401746350_wh1200.png"} alt={ele.name} className="w-12 h-12 rounded-full" /></td>
             <td>{ele.name}</td>
             <td>{ele.email}</td>
             <td>{ele.department}</td>
